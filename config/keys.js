133e086ebe9f8beda076099de464d58d600a46b5
Florent VICENTE <vicentegroupe@gmail.com>
2019-06-15 16:28:07 +0200

    Second commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
-}
+  mongoURI:
+    "mongodb+srv://StoryBook:plhbse43@cluster0-jhnit.mongodb.net/test?retryWrites=true&w=majority",
+  googleClientID:
+    "109504233771-blt0kjdm0e2s01oodalpm35mvji2413h.apps.googleusercontent.com",
+  googleClientSecret: "lv06XWrssRvTKfd9uSicFI24"
+};
